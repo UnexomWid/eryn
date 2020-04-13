99d194296957be0e35b667ef081221e8d967cf94
UnexomWid <uw@exom.dev>
2020-04-13 10:27:32 +0300

    [REFACTOR] N-API interaction function names

--- src/eryn.cxx
+++ src/eryn.cxx
@@ -8,24 +8,24 @@
 #include "compiler/compiler.hxx"
 #include "renderer/renderer.hxx"
 
-std::string JSONStringify(const Napi::Env& env, const Napi::Object& object) {
+std::string stringify(const Napi::Env& env, const Napi::Object& object) {
     Napi::Object json = env.Global().Get("JSON").As<Napi::Object>();
     Napi::Function stringify = env.Global().Get("JSON").As<Napi::Object>().Get("stringify").As<Napi::Function>();
 
     return stringify.Call(json, { object }).As<Napi::String>().Utf8Value();
 }
 
-Napi::Value Eval(const Napi::CallbackInfo& info) {
+Napi::Value eval(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
     Napi::String expression = info[0].As<Napi::String>();
-    std::string context = JSONStringify(env, info[1].As<Napi::Object>());
+    std::string context = stringify(env, info[1].As<Napi::Object>());
     env.RunScript("var context = " + context);
     Napi::Value x = env.RunScript(expression);
     return x;
 }
 
-void Compile(const Napi::CallbackInfo& info) {
+void compile(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
     const char* wd = info[0].As<Napi::String>().Utf8Value().c_str();
@@ -39,14 +39,14 @@ void Compile(const Napi::CallbackInfo& info) {
     }
 }
 
-void Render(const Napi::CallbackInfo& info) {
+void render(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
     const char* path = info[0].As<Napi::String>().Utf8Value().c_str();
     const char* outputPath = info[1].As<Napi::String>().Utf8Value().c_str();
     Napi::Object context = info[2].As<Napi::Object>();
 
-    env.RunScript("var context=" + JSONStringify(env, context));
+    env.RunScript("var context=" + stringify(env, context));
 
     try {
         renderFile(env, path, outputPath);
@@ -55,23 +55,23 @@ void Render(const Napi::CallbackInfo& info) {
     }
 }
 
-void Destroy(void* args) {
+void destroy(void* args) {
     Global::destroy();
 }
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
+Napi::Object init(Napi::Env env, Napi::Object exports) {
     printf("[eryn] Init\n");
     Global::init();
 
-    napi_add_env_cleanup_hook((napi_env) env, Destroy, nullptr);
+    napi_add_env_cleanup_hook((napi_env) env, destroy, nullptr);
 
     exports.Set(Napi::String::New(env, "eval"),
-                Napi::Function::New(env, Eval));
+                Napi::Function::New(env, eval));
     exports.Set(Napi::String::New(env, "compileFile"),
-                Napi::Function::New(env, Compile));
+                Napi::Function::New(env, compile));
     exports.Set(Napi::String::New(env, "renderFile"),
-                Napi::Function::New(env, Render));
+                Napi::Function::New(env, render));
     return exports;
 }
 
-NODE_API_MODULE(NODE_GYP_MODULE_NAME, Init)
+NODE_API_MODULE(NODE_GYP_MODULE_NAME, init)
