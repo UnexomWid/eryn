9b6fa8285e29a12db9f0e06930dec2decd41608e
UnexomWid <uw@exom.dev>
2020-05-03 15:23:34 +0300

    [ADD] Build check script

--- build-load.js
+++ build-load.js
@@ -7,11 +7,11 @@ const abi = process.versions.modules;
 const platform = os.platform();
 const arch = os.arch();
 
-function load (dir) {
-    return require(load.path(dir));
+function load (dir, prebuildMatcher, prebuildDirBuilder) {
+    return require(getPath(dir, prebuildMatcher, prebuildDirBuilder));
 }
 
-load.path = function (dir, prebuildMatcher, prebuildDirBuilder) {
+function getPath(dir, prebuildMatcher, prebuildDirBuilder) {
     dir = path.resolve(dir || '.');
 
     var release = findFirst(path.join(dir, 'build/Release'), matchBuild);
@@ -22,32 +22,36 @@ load.path = function (dir, prebuildMatcher, prebuildDirBuilder) {
     if (debug)
         return debug;
 
-    var prebuild = findFirst(path.join(dir, 'prebuilds/' + platform + '-' + arch), prebuildMatcher || matchNapiPrebuild);
+    var prebuild = findFirst(path.join(dir, (prebuildDirBuilder || buildDir)(runtime, abi, platform, arch)), prebuildMatcher || matchNapiPrebuild);
     if (prebuild)
         return prebuild;
 
     throw new Error(`No native build was found for runtime ${runtime} | ABI ${abi} | ${platform} platform | arch ${arch}`);
 }
 
+function buildDir(runtime, abi, platform, arch) {
+    return `prebuilds/${platform}-${arch}`;
+}
+
+function matchNapiPrebuild(name) {
+    let str = name.split('.')
+    return str[0] === runtime && str[1] === 'napi' && str[2] == 'node';
+}
+
 function findFirst(dir, filter) {
     try {
-        var files = fs.readdirSync(dir).filter(filter);
+        let files = fs.readdirSync(dir).filter(filter);
         return files[0] && path.join(dir, files[0])
     } catch (err) {
         return null;
     }
 }
 
-function matchNapiPrebuild (name) {
-    var str = name.split('.')
-    return str[0] === runtime && str[1] === 'napi' && str[2] == 'node';
-}
-
-function matchBuild (name) {
+function matchBuild(name) {
     return name.endsWith('.node');
 }
 
-function getRuntime () {
+function getRuntime() {
     if(process.versions && process.versions.electron)
         return 'electron';
     return (typeof window !== 'undefined'
