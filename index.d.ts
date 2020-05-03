8fd98487bb8eed09a2cda4ca3947ebdf13eb648d
UnexomWid <uw@exom.dev>
2020-05-03 18:36:12 +0300

    [MODIFY] Index files

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,16 @@
-// Type definitions for eryn 0.1.0
+// Type definitions for eryn 0.2.0
 // Definitions by UnexomWid <https://uw.exom.dev>
 
 export function compile(filePath : string) : void;
 export function compileDir(dirPath : string, filters : string[]) : void;
 export function render(filePath : string, context : object) : ArrayBuffer;
 export function setOptions(options : object) : void;
+
+declare const eryn: {
+    compile: (filePath : string) => void;
+    compileDir: (dirPath : string, filters : string[]) => void;
+    render: (filePath : string, context : object) => ArrayBuffer;
+    setOptions: (options : object) => void;
+};
+  
+export default eryn;
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var binding = require('./build-load')(__dirname);
 
-module.exports = binding;
+module.exports = {
+    compile: binding.compile,
+    compileDir: binding.compileDir,
+    render: binding.render,
+    setOptions: binding.setOptions,
+    default: binding
+};
