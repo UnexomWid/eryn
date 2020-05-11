006b10919a959ade0a1cee73831097f4af8bfd0e
UnexomWid <uw@exom.dev>
2020-05-11 17:07:59 +0300

    [ADD] More specific types for the setOptions argument

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,40 @@
 // Type definitions for eryn 0.2.0
 // Definitions by UnexomWid <https://uw.exom.dev>
 
+interface ErynOptions {
+    bypassCache?:              bool,
+    throwOnEmptyContent?:      bool,
+    throwOnMissingEntry?:      bool,
+    throwOnCompileDirError?:   bool,
+    ignoreBlankPlaintext?:     bool,
+    logRenderTime?:            bool,
+    workingDirectory?:         string,
+    templateEscape?:           string,
+    templateStart?:            string,
+    templateEnd?:              string,
+    conditionalStart?:         string,
+    conditionalEnd?:           string,
+    invertedConditionalStart?: string,
+    invertedConditionalEnd?:   string,
+    loopStart?:                string,
+    loopEnd?:                  string,
+    loopSeparator?:            string,
+    componentStart?:           string,
+    componentEnd?:             string,
+    componentSeparator?:       string,
+    componentSelf?:            string
+}
+
 export function compile(filePath : string) : void;
 export function compileDir(dirPath : string, filters : string[]) : void;
 export function render(filePath : string, context : object) : Buffer;
-export function setOptions(options : object) : void;
+export function setOptions(options : ErynOptions) : void;
 
 declare const eryn: {
     compile: (filePath : string) => void;
     compileDir: (dirPath : string, filters : string[]) => void;
     render: (filePath : string, context : object) => Buffer;
-    setOptions: (options : object) => void;
+    setOptions: (options : ErynOptions) => void;
 };
   
 export default eryn;
