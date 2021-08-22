af63fdb4a9564285d82844dbf505d220d443ac47
UnexomWid <uw@exom.dev>
2021-08-22 17:28:09 +0300

    [ADD] Cache method implementations

--- src/engine/cache.cxx
+++ src/engine/cache.cxx
@@ -1,15 +1,18 @@
 #include "engine.hxx"
 
-void Eryn::Cache::add(string key, Buffer value) {
-
+void Eryn::Cache::add(string key, Buffer&& value) {
+    // TODO: test if the move constructor is called.
+    entries[key] = value;
 }
 
 Buffer& Eryn::Cache::get(string key) {
     if(!has(key)) {
-        
+        throw ERYN_INTERNAL_EXCEPTION(("Cache item '" + key) + "' not found; get() must be guarded by has()");
     }
+
+    return entries[key];
 }
 
-bool Eryn::Cache::has(string key) {
-
+bool Eryn::Cache::has(string key) const {
+    return entries.find(key) != entries.end();
 }
