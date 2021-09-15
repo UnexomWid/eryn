2323cf39e33b549ee4b03b124f9a0661c9492af2
UnexomWid <uw@exom.dev>
2021-09-15 22:49:08 +0300

    [ADD] Strict bridge base

--- src/engine/bridge/bridge_strict.cxx
+++ src/engine/bridge/bridge_strict.cxx
@@ -21,72 +21,92 @@ static Napi::Value call_eval(Eryn::BridgeData& data, const Napi::String& script)
     }));
 }
 
-static Napi::Value call_eval(Eryn::BridgeData& data, ConstBuffer script) {
-    std::string str = std::string(reinterpret_cast<const char*>(script.data), script.size);
-    
-    // If the user writes {test: "Test"}, this should be treated as an expression.
-    // By default, it's treated as a block, but it will be treated as an expression if it's
-    // surrounded by parentheses. If the user truly wants the script to start with a block,
-    // they have to place dummy content like /**/ or /* Block */ before the opening bracket.
-    if(script.size > 0 && script.data[0] == '{') {
-        str ="(" + str + ")";
-    }
-
-    return call_eval(data, Napi::String::New(data.env, str));
-}
-
 static Napi::Value call_clone(Eryn::BridgeData& data, const Napi::Value& original) {
     return data.clone.Call(std::initializer_list<napi_value>({
         original
     }));
 }
 
-Eryn::NormalBridge::NormalBridge(Eryn::BridgeData&& data) : Bridge(std::forward<Eryn::BridgeData>(data)) { }
+// Evaluates a piece of script to a Napi::Value.
+static Napi::Value eval(Eryn::BridgeData& data, ConstBuffer script) {
+    Napi::Value baseValue;
 
-void Eryn::NormalBridge::evalTemplate(ConstBuffer input, Buffer& output) {
+    if(script.match("context", sizeof("context") - 1)) {
+        baseValue = data.context;
+    } else if(script.match("local", sizeof("local") - 1)) {
+        baseValue = data.local;
+    } else if(script.match("shared"), sizeof("shared") - 1) {
+        baseValue = data.shared;
+    } else {
+        throw Eryn::RenderingException("Template content is too complex", "'strict' mode only supports simple content such as 'context.fieldName'. Consider using 'normal' mode", script);
+    }
+
+    auto dot = script.find(".", 1);
+
+    // Dot not found, return the base value itself.
+    if(dot == script.end()) {
+        return baseValue;
+    }
+
+    // Dot found, return the value of the field if the base value is an object.
+    if(!baseValue.IsObject()) {
+        std::string baseName(reinterpret_cast<const char*>(script.data), dot - script.data);
+
+        throw Eryn::RenderingException("Cannot access field of non-object value", ("make sure '" + baseName + "' is an object").c_str(), script);
+    }
+
+    auto base  = baseValue.As<Napi::Object>();
+    auto field = Napi::String::New(data.env, std::string(reinterpret_cast<const char*>(dot), script.size - (dot - script.data)));
+
+    return base.Get(field);
+}
+
+Eryn::StrictBridge::StrictBridge(Eryn::BridgeData&& data) : Bridge(std::forward<Eryn::BridgeData>(data)) { }
+
+void Eryn::StrictBridge::evalTemplate(ConstBuffer input, Buffer& output) {
     Napi::Value result;
 
     try {
-        result = call_eval(data, input);
+        result = eval(data, input);
     } catch(std::exception &e) {
         throw Eryn::RenderingException("Template error", e.what(), input);
     }
 
-    if(result.IsUndefined() || result.IsNull())
+    if(result.IsUndefined() || result.IsNull()) {
         return;
-    else if(result.IsString()) {
+    } else if(result.IsString()) {
         LOG_DEBUG("    Type: string");
 
-        std::string str = result.As<Napi::String>().Utf8Value();
-        const uint8_t* ptr = reinterpret_cast<const uint8_t*>(str.c_str());
+        auto str = result.As<Napi::String>().Utf8Value();
+        auto ptr = reinterpret_cast<const uint8_t*>(str.c_str());
 
         output.write(ptr, str.size());
     } else if(result.IsBuffer()) {
         LOG_DEBUG("    Type: buffer");
 
-        uint8_t* ptr = reinterpret_cast<uint8_t*>(result.As<Napi::Buffer<char>>().Data());
-        size_t length = result.As<Napi::Buffer<char>>().Length();
+        auto ptr = reinterpret_cast<uint8_t*>(result.As<Napi::Buffer<char>>().Data());
+        auto length = result.As<Napi::Buffer<char>>().Length();
 
         output.write(ptr, length);
     } else if(result.IsObject()) {
         LOG_DEBUG("    Type: object");
 
-        std::string str = stringify(data.env, result.As<Napi::Object>());
-        const uint8_t* ptr = reinterpret_cast<const uint8_t*>(str.c_str());
+        auto str = stringify(data.env, result.As<Napi::Object>());
+        auto ptr = reinterpret_cast<const uint8_t*>(str.c_str());
 
         output.write(ptr, str.size());
     } else if(result.IsArray()) {
         LOG_DEBUG("    Type: array");
 
-        std::string str = stringify(data.env, result.ToObject());
-        const uint8_t* ptr = reinterpret_cast<const uint8_t*>(str.c_str());
+        auto str = stringify(data.env, result.ToObject());
+        auto ptr = reinterpret_cast<const uint8_t*>(str.c_str());
 
         output.write(ptr, str.size());
     } else if(result.IsNumber()) {
         LOG_DEBUG("    Type: number");
 
-        std::string str = result.ToString().Utf8Value();
-        const uint8_t* ptr = reinterpret_cast<const uint8_t*>(str.c_str());
+        auto str = result.ToString().Utf8Value();
+        auto ptr = reinterpret_cast<const uint8_t*>(str.c_str());
 
         output.write(ptr, str.size());
     } else {
@@ -94,19 +114,15 @@ void Eryn::NormalBridge::evalTemplate(ConstBuffer input, Buffer& output) {
     }
 }
 
-void Eryn::NormalBridge::evalVoidTemplate(ConstBuffer input) {
-    try {
-        call_eval(data, input);
-    } catch(std::exception &e) {
-        throw Eryn::RenderingException("Void template error", e.what(), input);
-    }
+void Eryn::StrictBridge::evalVoidTemplate(ConstBuffer input) {
+    throw Eryn::RenderingException("Unsupported template type", "void templates are not supported in 'strict' mode", input);
 }
 
-bool Eryn::NormalBridge::evalConditionalTemplate(ConstBuffer input) {
+bool Eryn::StrictBridge::evalConditionalTemplate(ConstBuffer input) {
     Napi::Value result;
 
     try {
-        result = call_eval(data, input);
+        result = eval(data, input);
     } catch(std::exception &e) {
         throw Eryn::RenderingException("Conditional template error", e.what(), input);
     }
@@ -114,7 +130,7 @@ bool Eryn::NormalBridge::evalConditionalTemplate(ConstBuffer input) {
     return result.ToBoolean().Value();
 }
 
-void Eryn::NormalBridge::evalAssignment(bool cloneIterators, const std::string& iterator, const std::string& assignment, const std::string& propertyAssignment) {
+void Eryn::StrictBridge::evalAssignment(bool cloneIterators, const std::string& iterator, const std::string& assignment, const std::string& propertyAssignment) {
     if(cloneIterators) {
         // Object
         if(propertyAssignment.size() > 0) {
@@ -145,69 +161,26 @@ void Eryn::NormalBridge::evalAssignment(bool cloneIterators, const std::string& 
     }
 }
 
-void Eryn::NormalBridge::unassign(const std::string &iterator) {
+void Eryn::StrictBridge::evalIteratorArrayAssignment(bool cloneIterators, const std::string& iterator, const BridgeIterable& iterable, const BridgeObjectKeys& keys, uint32_t index) {
+    
+}
+
+void Eryn::StrictBridge::evalIteratorObjectAssignment(bool cloneIterators, const std::string& iterator, const Eryn::BridgeIterable& iterable, const Eryn::BridgeObjectKeys& keys, uint32_t index) {
+    
+}
+
+bool Eryn::StrictBridge::initLoopIterable(ConstBuffer arrayScript, Eryn::BridgeIterable& iterable, Eryn::BridgeObjectKeys& keys, int8_t step) {
+    return false;
+}
+
+void Eryn::StrictBridge::unassign(const std::string &iterator) {
     data.local[iterator] = call_eval(
         data,
         Napi::String::New(data.env, "undefined")
     );
 }
 
-size_t Eryn::NormalBridge::initArray(ConstBuffer array, std::string*& propertyArray, int8_t direction) {
-    Napi::Value result;
-
-    try {
-        result = call_eval(data, array);
-    } catch(std::exception &e) {
-        throw Eryn::RenderingException("Loop template error", e.what(), array);
-    }
-
-    bool isSimpleArray = true;
-
-    if(result.IsArray()) {
-        return result.As<Napi::Array>().Length();
-    }
-    
-    if(!result.IsObject()) {
-        throw Eryn::RenderingException("Unsupported loop right operand", "must be Array or Object", array);
-    }
-
-    Napi::Array properties = result.ToObject().GetPropertyNames();
-
-    // TODO: render nothing?
-    if(properties.Length() == 0) {
-        throw RenderingException("Unsupported loop right operand", "length is 0", array);
-    }
-
-    propertyArray = new std::string[properties.Length()];
-
-    if(direction > 0) {
-        for(uint32_t i = 0; i < properties.Length(); ++i) {
-            propertyArray[i] = ((Napi::Value) properties[i]).As<Napi::String>().Utf8Value();
-
-            if(((Napi::Value) properties[i]).As<Napi::String>().Utf8Value() != std::to_string(i))
-                isSimpleArray = false; // Object.
-        }
-    } else {
-        for(uint32_t i = properties.Length() - 1; ; --i) {
-            propertyArray[i] = ((Napi::Value) properties[i]).As<Napi::String>().Utf8Value();
-
-            if(((Napi::Value) properties[i]).As<Napi::String>().Utf8Value() != std::to_string(i))
-                isSimpleArray = false; // Object.
-
-            if(i == 0) // Unsignedness causes overflow (always >= 0), so break manually.
-                break;
-        }
-    }
-
-    if(isSimpleArray) {
-        delete[] propertyArray;
-        propertyArray = nullptr;
-    }
-
-    return properties.Length();
-}
-
-void Eryn::NormalBridge::buildLoopAssignment(std::string& iterator, std::string& assignment, size_t& assignmentUpdateIndex, ConstBuffer it, ConstBuffer array) {
+void Eryn::StrictBridge::buildLoopAssignment(std::string& iterator, std::string& assignment, size_t& assignmentUpdateIndex, ConstBuffer it, ConstBuffer array) {
     iterator.assign(reinterpret_cast<const char*>(it.data), it.size);
     
     assignment.reserve(32);
@@ -219,7 +192,7 @@ void Eryn::NormalBridge::buildLoopAssignment(std::string& iterator, std::string&
 
 // Assignment: iterator = arr[index]
 // Assignment: iterator = Object({key: prop, value: obj["prop"]})
-void Eryn::NormalBridge::updateLoopAssignment(std::string& assignment, std::string& propertyAssignment, size_t& arrayIndex, std::string*& propertyArray, int8_t direction) {
+void Eryn::StrictBridge::updateLoopAssignment(std::string& assignment, std::string& propertyAssignment, size_t& arrayIndex, std::string*& propertyArray, int8_t direction) {
     if(propertyArray == nullptr) {
         assignment += std::to_string(arrayIndex);
         assignment += "]";
@@ -231,12 +204,12 @@ void Eryn::NormalBridge::updateLoopAssignment(std::string& assignment, std::stri
     arrayIndex += direction;
 }
 
-void Eryn::NormalBridge::invalidateLoopAssignment(std::string& assignment, const size_t& assignmentUpdateIndex) {
+void Eryn::StrictBridge::invalidateLoopAssignment(std::string& assignment, const size_t& assignmentUpdateIndex) {
     assignment.erase(assignmentUpdateIndex, assignment.size() - assignmentUpdateIndex);
 }
 
 // Like call_clone, but this one is exposed by the bridge and also catches any exceptions.
-Eryn::BridgeBackup Eryn::NormalBridge::copyValue(const Napi::Value& value) {
+Eryn::BridgeBackup Eryn::StrictBridge::copyValue(const Napi::Value& value) {
     try {
         return call_clone(data, value);
     } catch(std::exception& e) {
@@ -247,7 +220,7 @@ Eryn::BridgeBackup Eryn::NormalBridge::copyValue(const Napi::Value& value) {
     }
 }
 
-Eryn::BridgeBackup Eryn::NormalBridge::backupContext(bool cloneBackup) {
+Eryn::BridgeBackup Eryn::StrictBridge::backupContext(bool cloneBackup) {
     try {
         if(cloneBackup) {
             return call_clone(data, data.context);
@@ -261,7 +234,7 @@ Eryn::BridgeBackup Eryn::NormalBridge::backupContext(bool cloneBackup) {
     }
 }
 
-Eryn::BridgeBackup Eryn::NormalBridge::backupLocal(bool cloneBackup) {
+Eryn::BridgeBackup Eryn::StrictBridge::backupLocal(bool cloneBackup) {
     try {
         if(cloneBackup) {
             return call_clone(data, data.local);
@@ -275,7 +248,7 @@ Eryn::BridgeBackup Eryn::NormalBridge::backupLocal(bool cloneBackup) {
     }
 }
 
-void Eryn::NormalBridge::initContext(ConstBuffer context) {
+void Eryn::StrictBridge::initContext(ConstBuffer context) {
     try {
         if(context.size == 0) {
             data.context = call_eval(
@@ -283,17 +256,17 @@ void Eryn::NormalBridge::initContext(ConstBuffer context) {
                 Napi::String::New(data.env, "Object({})")
             );//.ToObject();
         } else {
-            data.context = call_eval(
-                data,
-                context
-            );//.As<Napi::Object>();
+            ///data.context = call_eval(
+            ///    data,
+            ///    context
+            ///);//.As<Napi::Object>();
         }
     } catch(std::exception &e) {
         throw Eryn::RenderingException("Component template error", (std::string("context: ") + e.what()).c_str(), context);
     }
 }
 
-void Eryn::NormalBridge::initLocal() {
+void Eryn::StrictBridge::initLocal() {
     try {
         data.local = call_eval(
             data,
@@ -304,10 +277,10 @@ void Eryn::NormalBridge::initLocal() {
     }
 }
 
-void Eryn::NormalBridge::restoreContext(Eryn::BridgeBackup backup) {
+void Eryn::StrictBridge::restoreContext(Eryn::BridgeBackup backup) {
     data.context = backup;//.ToObject();
 }
 
-void Eryn::NormalBridge::restoreLocal(Eryn::BridgeBackup backup) {
+void Eryn::StrictBridge::restoreLocal(Eryn::BridgeBackup backup) {
     data.local = backup.ToObject();
 }
