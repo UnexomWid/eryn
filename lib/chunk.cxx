895ec9b1ba973de2084ed9622105399931e14c1d
UnexomWid <uw@exom.dev>
2022-01-07 11:49:34 +0200

    [FIX] Compiler error chunk bug

--- lib/chunk.cxx
+++ lib/chunk.cxx
@@ -9,26 +9,27 @@ Chunk::Chunk(const uint8_t* source, size_t sourceSize, size_t index, size_t maxC
     uint8_t current;
 
     const uint8_t* limit = source + index;
+    const uint8_t* ptr = source;
 
-    while(source < limit) {
-        current = *(source++);
+    while(ptr < limit) {
+        current = *(ptr++);
 
         if(current == '\n') {
             ++this->line;
             this->column = 1;
 
-            // Source was already incremented. Check for \r and ignore it,
+            // Pointer was already incremented. Check for \r and ignore it,
             // such that this can work with \r, \n, \r\n, and \n\r
-            if(source < limit && *source == '\r') {
-                ++source;
+            if(ptr < limit && *ptr == '\r') {
+                ++ptr;
             }
         } else if(current == '\r') {
             ++this->line;
             this->column = 1;
 
             // Same as above.
-            if(source < limit && *source == '\n') {
-                ++source;
+            if(ptr < limit && *ptr == '\n') {
+                ++ptr;
             }
         } else {
             ++this->column;
