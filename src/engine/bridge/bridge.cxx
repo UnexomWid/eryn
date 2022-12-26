140073bdfede37d0b8d3fa18fd1f3550c400d260
UnexomWid <uw@exom.dev>
2022-12-26 22:35:18 +0200

    update: windows prebuilds

--- src/engine/bridge/bridge.cxx
+++ src/engine/bridge/bridge.cxx
@@ -2,6 +2,8 @@
 
 #include "bridge.hxx"
 
+#include "../../def/logging.dxx"
+
 Eryn::BridgeCompileData Eryn::Bridge::to_compile_data() {
     return Eryn::BridgeCompileData(data.env);
 }
@@ -13,6 +15,8 @@ bool Eryn::Bridge::call_hook(BridgeCompileData data, BridgeHook& hook, Buffer& i
     auto result = hook.Call(std::initializer_list<napi_value>({ buff, originStr }));
 
     if (result.IsString()) {
+        LOG_DEBUG("Hook result is string");
+
         auto str = result.As<Napi::String>().Utf8Value();
         auto ptr = reinterpret_cast<const uint8_t*>(str.c_str());
 
@@ -21,6 +25,8 @@ bool Eryn::Bridge::call_hook(BridgeCompileData data, BridgeHook& hook, Buffer& i
 
         return true;
     } else if (result.IsBuffer()) {
+        LOG_DEBUG("Hook result is buffer");
+
         auto ptr    = reinterpret_cast<uint8_t*>(result.As<Napi::Buffer<char>>().Data());
         auto length = result.As<Napi::Buffer<char>>().Length();
 
@@ -29,8 +35,11 @@ bool Eryn::Bridge::call_hook(BridgeCompileData data, BridgeHook& hook, Buffer& i
 
         return true;
     } else if (!result.IsUndefined() && !result.IsNull()) {
+        LOG_DEBUG("Hook result is invalid");
         return false;
     }
+
+    LOG_DEBUG("Hook result is undefined or null");
 
     return true;
 }
